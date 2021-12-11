297e32cc17fee605e2dedefeda9367326ae5ea53
Netia Ingram <c-netia.ingram@charter.com>
2021-12-11 16:00:48 -0700

    Add ability to delete cards from view

--- javascript.js
+++ javascript.js
@@ -1,12 +1,39 @@
   let cards = [];
+  let filterArray = [];
+
+  function deleteCard(id) {
+
+    filterArray.push(id);
+    let content = '';
+
+    cards.filter(card => {
+      if(!filterArray.includes(card.id)) {
+        content += `<article class='card'> 
+        <figure class='card-item' style='background-image: url(${card.imageUrl})'>
+        </figure>
+        <summary class='card-details card-item''>
+        <h5>${card.header}</h5>
+        <p>${card.details}</p>
+        <p id="delete-${card.id}" style="color: darkred; text-decoration: underline" onClick="deleteCard(${card.id});"> Delete</p>
+        </summary>
+        </article>`;
+        }
+      });
+
+    let contentSection = document.getElementById("stupid-section")
+
+    contentSection.innerHTML = '';
+    contentSection.innerHTML = content
+    
+  }
   
-  function generateNewCardDeck() {
-    const numCards = Math.floor(Math.random() * 9);
+  function generateNewCardDeck(numCards = Math.floor(Math.random() * 9)) {
+    cards = [];
+
     for (let i = numCards; i > 0; i--) {
       const card = new Card(i);
-      cards.push(card);
+      cards.unshift(card);
     }
-
     let content = '';
 
     cards.forEach(card => {
@@ -16,11 +43,13 @@
                     <summary class='card-details card-item''>
                       <h5>${card.header}</h5>
                       <p>${card.details}</p>
+                      <p id="delete-${card.id}" style="color: darkred; text-decoration: underline" onClick="deleteCard(${card.id});"> Delete</p>
                     </summary>
                   </article>`;
       });
 
     let contentSection = document.getElementById("stupid-section")
+
     contentSection.innerHTML = content
      
   }
